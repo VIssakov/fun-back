24ba2a2d25c1c047315b32c0800ddd29eadc6961
Валерий В. Исаков <isakovvv@yoomoney.ru>
2022-06-24 02:26:37 +0300

    two

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -52,6 +52,7 @@ def forward_to_target_url(url_key: str, request: Request, db: Session = Depends(
 @app.get("/admin/{secret_key}", name="administration info", response_model=schemas.URLInfo,)
 def get_url_info(secret_key: str, request: Request, db: Session = Depends(get_db)):
     if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
+        crud.update_db_clicks(db=db, db_url=db_url)
         return get_admin_info(db_url)
     else:
         raise_not_found(request)
@@ -63,3 +64,13 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
 
     db_url = crud.create_db_url(db=db, url=url)
     return get_admin_info(db_url)
+
+@app.delete("/admin/{secret_key}")
+def delete_url(
+    secret_key: str, request: Request, db: Session = Depends(get_db)
+):
+    if db_url := crud.deactivate_db_url_by_secret_key(db, secret_key=secret_key):
+        message = f"Successfully deleted shortened URL for '{db_url.target_url}'"
+        return {"detail": message}
+    else:
+        raise_not_found(request)
